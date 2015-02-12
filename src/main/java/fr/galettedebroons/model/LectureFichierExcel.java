53400053fcd1a0afe145af35310f2b8b42dad46e
poher <pohermelissa@gmail.com>
2015-02-12 17:21:33 +0100

    Mise a jour de ma lecture de fichier Creation en base de donnée

--- src/main/java/fr/galettedebroons/model/LectureFichierExcel.java
+++ src/main/java/fr/galettedebroons/model/LectureFichierExcel.java
@@ -4,8 +4,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -17,6 +24,9 @@ import fr.galettedebroons.view.FormulaireGalette;
 
 
 public class LectureFichierExcel {
+	
+	private static EntityManager manager;
+	
 	private static String nomDocument_;
 	private static Date date_;
 	private static String codeClient_;
@@ -27,16 +37,22 @@ public class LectureFichierExcel {
 	//private static Logger logger = Logger.getLogger(Lecture_Fichier_Excel.class);
 	
 	
+	public LectureFichierExcel(EntityManager manager){
+		this.manager = manager;
+	}
+	
+	
+	
 	/**
 	 * open and read excel file
 	 * @throws InvalidFormatException
 	 */
-	public void ouverture_fichier(File file) throws InvalidFormatException {
-	//public static void main(String[] args) throws InvalidFormatException{	
+	//public void ouverture_fichier(File file) throws InvalidFormatException {
+	public static void main(String[] args) throws InvalidFormatException{	
 		System.out.println("HELLO !!!");
 	    try {
-	    	new FormulaireGalette().setVisible(true);
-	    	//File file = new File(("C:\\exemple1.xlsx"));
+	    	//new FormulaireGalette().setVisible(true);
+	    	File file = new File(("C:\\Classeur1.xlsx"));
 	    	
 	        final Workbook workbook = WorkbookFactory.create(file);
 	        System.out.println("J'ai trouve mon fichier !!!");
@@ -57,7 +73,9 @@ public class LectureFichierExcel {
 	        	date_ = row.getCell(1).getDateCellValue();
 	        	codeClient_ = row.getCell(2).getStringCellValue();
 	        	nomClient_ = row.getCell(3).getStringCellValue();
-	        	codeArticle_ = row.getCell(4).getStringCellValue();
+	        	// convert row to string exception when cell is a int
+	        	row.getCell(4).setCellType(Cell.CELL_TYPE_STRING);
+	        	codeArticle_ = (String) row.getCell(4).getStringCellValue();
 	        	quantite_ = (int) row.getCell(5).getNumericCellValue();
 	        	
 	        	System.out.println("Le nom du document : " +nomDocument_);
@@ -67,6 +85,21 @@ public class LectureFichierExcel {
 	        	System.out.println("Le code de l'article : " +codeArticle_);
 	        	System.out.println("La quantite : " +quantite_);
 	        	
+	        	
+	        	// add to database with read values
+	        	EntityManagerFactory factory = Persistence.createEntityManagerFactory("example");
+	        	EntityManager manager = factory.createEntityManager();
+	        	LectureFichierExcel test = new LectureFichierExcel(manager);
+	        	
+	        	EntityTransaction tx = manager.getTransaction();
+	        	tx.begin();
+	        	
+	        	try{
+	        		insertLigneClient();
+	        	}catch(Exception e){
+	        		e.printStackTrace();
+	        	}  	
+	        	
 	            row = sheet.getRow(index++);
 	        }
 	
@@ -75,6 +108,21 @@ public class LectureFichierExcel {
 	    } catch (IOException e) {
 	        e.printStackTrace();
 	    }
+	}
+	
+	
+	public static void insertLigneClient(){
+		// Verification si le client n'est pas dans la base
+		List<Client> client = manager.createQuery("select c from Client c where code_client = " +codeClient_, Client.class).getResultList();
+		if (client.size() == 0){
+			// Afficher la fenetre créer un client
+			// Vous devez renseigner les champs du nouveau client
+			// Client inexistant
+		}else {
+			// J'ajoute ma ligne de livraison
+			Livraison_Effective lf = new Livraison_Effective(nomDocument_, date_, codeClient_, nomClient_, codeArticle_,quantite_);
+		}
+		
 	}
 	
 	public String getNom_document() {
