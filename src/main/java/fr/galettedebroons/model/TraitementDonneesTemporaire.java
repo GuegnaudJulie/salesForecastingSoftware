472a1bab79f3b5c8cffeb4e9cc1637855cf21faf
GuegnaudJulie <julie.guegnaud@gmail.com>
2015-03-23 12:41:58 +0100

    MAJ TraitementDonneesTempoarire

--- src/main/java/fr/galettedebroons/model/TraitementDonneesTemporaire.java
+++ src/main/java/fr/galettedebroons/model/TraitementDonneesTemporaire.java
@@ -4,13 +4,15 @@ import javax.swing.JOptionPane;
 
 import fr.galettedebroons.test.Main;
 import fr.galettedebroons.view.PanelEdition;
+import fr.galettedebroons.view.VueGlobalNvClient;
+import fr.galettedebroons.view.VueGlobalNvProduit;
 
 public class TraitementDonneesTemporaire {
 
 	private Main main_;
 	private PanelEdition panel_;
-	//private JFrame fenetreClient;
-	//private JFrame fenetreProduit;
+	private VueGlobalNvClient fenetreClient;
+	private VueGlobalNvProduit fenetreProduit;
 	
 	public TraitementDonneesTemporaire(Main main, PanelEdition panel){
 		main_ = main;
@@ -25,57 +27,38 @@ public class TraitementDonneesTemporaire {
 	 * Insertion dans livraisons
 	 */
 	public void insertionDonnee(){
-		/* DANS THIS
-		fenetreClient = new JFrame();
-		fenetreClient.setSize(700,500);
-		fenetreClient.setLocationRelativeTo(null);
-		VueGlobalNvClient creaClients = new VueGlobalNvClient(this);
-		JScrollPane pane = new JScrollPane(creaClients);
-		fenetreClient.setContentPane(pane);
-		fenetreClient.setVisible(true);
-		*/
+
+		//fenetreClient = new VueGlobalNvClient(this);
 		
 		/* DANS VueGlobalNvClient
+		
+		private TraitementDonneesTemporaire ClasseTraitement_;
+		
 		private static void enregistrercliActionPerformed(ActionEvent evt) {
-			//Ajout à la fin
-			closeClient();
+			//Ajout Ã  la fin
+			ClasseTraitement_.insertionProduit();
 		}
 		*/
 	}
 	
-	public void closeClient(){
-		//fenetreClient.setVisible(false);
-		insertionDonneeSuite();
-	}
-	
-	public void insertionDonneeSuite(){
-		/* DANS THIS
-		fenetreProduit = new JFrame();
-		fenetreProduit.setSize(700,500);
-		fenetreProduit.setLocationRelativeTo(null);
-		VueGlobalNvProduit creaProduits = new VueGlobalNvProduit(this);
-		JScrollPane pane = new JScrollPane(creaProduits);
-		fenetreProduit.setContentPane(pane);
-		fenetreProduit.setVisible(true);
-		*/
+	public void insertionProduit(){
+		//fenetreProduit = new VueGlobalNvProduit(this);
 		
 		/* DANS VueGlobalNvProduit
+		
+		private TraitementDonneesTemporaire ClasseTraitement_;
+		
 		private static void enregistrercliActionPerformed(ActionEvent evt) {
-			//Ajout à la fin
-			closeProduit();
+			//Ajout Ã  la fin
+			ClasseTraitement_.insertionDonneeFin();
 		}
 		*/
-	}
-	
-	public void closeProduit(){
-		//fenetreProduit.setVisible(false);
-		insertionDonneeFin();
 	}
 	
 	public void insertionDonneeFin(){
 		new RemplissageLivraison(main_);
 		RangerDonneeTemporaire rdt = new RangerDonneeTemporaire(main_);
-		JOptionPane.showMessageDialog(panel_, "Les livraisons ont été ajoutées");
+		JOptionPane.showMessageDialog(panel_, "Les livraisons ont Ã©tÃ© ajoutÃ©es");
 		rdt.vidage();
 	}
 }
