ce2f80511d9af21ea240a536f4ecabd71687d6fa
GuegnaudJulie <julie.guegnaud@gmail.com>
2016-02-23 10:13:38 +0100

    Re-fin

---
+++ run.bat
@@ -0,0 +1,4 @@
+#java -Djava.library.path="C:\R\library\rJava\jri\x64" -jar salesForecastingSoftware-0.0.1-SNAPSHOT.jar
+SET R_LIBS=c:/R/library/
+SET R_HOME=c:/R/
+java -Djava.library.path="C:\R\library\rJava\jri\x64" -jar salesForecastingSoftware-0.0.1-SNAPSHOT.jar
